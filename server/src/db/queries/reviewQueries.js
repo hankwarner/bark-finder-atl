28123345963f701dd9df37dbe1929b93ccb28961
hankwarner <40283259+hankwarner@users.noreply.github.com>
2019-02-06 00:17:05 -0500

    WIP

---
+++ server/src/db/queries/reviewQueries.js
@@ -0,0 +1,17 @@
+const User = require('../models').User
+const Park = require('../models').Park
+
+module.exports = {
+
+    // async getReview(callback) {
+    //     try {
+    //         let reviews = await Review.
+
+    //         return callback(null, reviews)
+            
+    //     } catch(error) {
+    //         console.log(error)
+    //         callback(error)
+    //     }
+    // }
+}
