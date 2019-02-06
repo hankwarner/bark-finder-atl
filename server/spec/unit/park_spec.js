932e1794e01f09da9f7d51a18d06561f4d71d447
hankwarner <40283259+hankwarner@users.noreply.github.com>
2019-02-06 00:40:30 -0500

    added getReview unit spec

--- server/spec/unit/park_spec.js
+++ server/spec/unit/park_spec.js
@@ -1,21 +1,43 @@
 const sequelize = require("../../src/db/models/index").sequelize;
+const User = require("../../src/db/models").User;
 const Park = require("../../src/db/models").Park;
+const Review = require("../../src/db/models").Review;
 
 describe('Parks', () => {
     
     beforeEach((done) => {
+        this.user;
         this.park;
    
         sequelize.sync({force: true}).then((res) => {
-            Park.create({
-              name: "Mission Dolores Park",
-              address: '19th Street',
-              neighborhood: 'Mission',
-              description: "Leave no trace"
+            User.create({
+                username: "wolverine",
+                email: "wolverine@xmen.com",
+                password: "00x00x00"
             })
-            .then((park) => {
-              this.park = park;
-              done();
+            .then((user) => {
+                this.user = user;
+
+                Park.create({
+                    name: "Mission Dolores Park",
+                    address: '19th Street',
+                    neighborhood: 'Mission',
+                    description: "Leave no trace",
+                    reviews: [{
+                        userId: this.user.id,
+                        body: "Great spot",
+                        rating: 5
+                    }]
+                }, {
+                    include: {
+                        model: Review,
+                        as: "reviews"
+                    }
+                })
+                .then((park) => {
+                    this.park = park;
+                    done();
+                })
             })
         })
       })
@@ -29,5 +51,18 @@ describe('Parks', () => {
             expect(this.park.description).toContain("Leave no trace");
             done();
         })  
+    })
+
+    describe('#getReviews()', () => {
+        it('should return all reviews with the park in scope', (done) => {
+            this.park.getReviews()
+            .then((associatedReviews) => {
+                expect(associatedReviews[0].body).toContain("Great spot");
+                expect(associatedReviews[0].rating).toEqual(5);
+                expect(associatedReviews[0].parkId).toEqual(this.park.id);
+                expect(associatedReviews[0].userId).toEqual(this.user.id);
+                done();
+            })
+        })
     })
 })
--- server/src/db/queries/parkQueries.js
+++ server/src/db/queries/parkQueries.js
@@ -1,4 +1,5 @@
 const Park = require('../models').Park
+const Review = require('../models').Review
 
 module.exports = {
 
@@ -11,5 +12,20 @@ module.exports = {
             console.log(error)
             callback(error)
         }
+    },
+
+    async getPark(id, callback){
+        return Park.findById(id, {
+          include: [  {
+            model: Review,
+            as: "reviews"
+          }]
+        })
+        .then((park) => {
+          callback(null, park);
+        })
+        .catch((err) => {
+          callback(err);
+        })
     }
 }
