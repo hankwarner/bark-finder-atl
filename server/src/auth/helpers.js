3151e99b7a5b7be396d81529e68943cdd15ddcf9
hankwarner <40283259+hankwarner@users.noreply.github.com>
2019-02-12 22:01:19 -0500

    added token validation to createReview

--- server/src/auth/helpers.js
+++ server/src/auth/helpers.js
@@ -2,13 +2,13 @@ const bcrypt = require("bcryptjs");
 
 module.exports = {
 
-  // ensureAuthenticated(req, res, next) {
-  //   if (!req.user){
-  //     req.flash("notice", "You must be signed in to do that.")
-  //   } else {
-  //     next();
-  //   }
-  // },
+  ensureAuthenticated(req, res, next) {
+    if (!req.body.token){
+      res.status(400).send({message: 'You must be signed in to do that.'})
+    } else {
+      next();
+    }
+  },
 
   comparePass(userPassword, databasePassword) {
     if (userPassword === null) {
--- server/src/routes.js
+++ server/src/routes.js
@@ -2,12 +2,13 @@ const AuthenticationController = require('./controllers/AuthenticationController
 const ParkController = require('./controllers/ParkController')
 const ReviewController = require('./controllers/ReviewController')
 const validation = require("./validation")
+const helper = require("./auth/helpers")
 
 module.exports = (app) => {
     app.post('/register', validation.validatePasswords, AuthenticationController.register)
     app.post('/login', AuthenticationController.login),
     app.get('/parks', ParkController.index),
     app.get('/parks/:id', ParkController.show),
-    app.post('/parks/:id/review/create', ReviewController.create)
+    app.post('/parks/:id/review/create', helper.ensureAuthenticated, ReviewController.create)
     app.post('/parks/:id/review/destroy', ReviewController.destroy)
 }
