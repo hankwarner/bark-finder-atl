e7963b75a39e1d95d08a36fc13bae8b15de3ad72
hankwarner <40283259+hankwarner@users.noreply.github.com>
2019-02-12 23:31:00 -0500

    adding heroku add to api for prod environment

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -1,10 +1,18 @@
 import axios from 'axios'
 
 export default () => {
-    return axios.create({
-        baseURL: 'http://localhost:8081',
-        // headers: {
-        //     Authorization: `Bearer ${store.state.token}`
-        // }
-    })
+    //need to use the heroku URL here for production environment
+    if(process.env.NODE_ENV === 'development') {
+        return axios.create({
+            baseURL: 'http://localhost:8081'
+        })
+    } else {
+        return axios.create({
+            baseURL: 'https://bark-finder-atl.herokuapp.com'
+        })
+    }
 }
+
+// headers: {
+    //     Authorization: `Bearer ${store.state.token}`
+    // }
