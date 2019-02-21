42ac495687d62f8601d3c188225fe7e28bb02270
hankwarner <40283259+hankwarner@users.noreply.github.com>
2019-02-20 23:37:16 -0500

    added card styles to parks

--- client/src/components/Parks.vue
+++ client/src/components/Parks.vue
@@ -1,6 +1,76 @@
-<template>
-    
-  <v-container fluid>
+<template> 
+<v-container fluid>
+  <v-layout row wrap justify-space-around>
+    <v-flex 
+      d-flex 
+      xs12 
+      md4
+      mx-5
+      my-4
+      v-for="(park) in parks"
+      v-bind:key="park.id"
+    >
+      <v-hover>
+        <v-card
+          slot-scope="{ hover }"
+          class="mx-auto"
+          color="grey lighten-4"
+          max-width="600"
+        >
+          <v-img
+            :aspect-ratio="16/9"
+            :src="park.imageUrl"
+            @click="navigateTo({
+              name: 'park',
+              params: {
+                parkId: park.id
+              }
+            })"
+          >
+            <v-expand-transition>
+              <div
+                v-if="hover"
+                class="d-flex transition-fast-in-fast-out orange darken-2 v-card--reveal display-3 white--text"
+                style="height: 100%;"
+              >
+                {{park.neighborhood}}
+              </div>
+            </v-expand-transition>
+          </v-img>
+          <v-card-text
+            class="pt-4"
+            style="position: relative;"
+          >
+            <v-btn
+              absolute
+              color="orange"
+              class="white--text"
+              fab
+              large
+              right
+              top
+              @click="navigateTo({
+              name: 'park',
+              params: {
+                parkId: park.id
+              }
+            })"
+            >
+              <v-icon>mdi-cart</v-icon>
+            </v-btn>
+            <h3 class="display-1 font-weight-light orange--text mb-2">{{park.name}}</h3>
+            <div class="font-weight-light title mb-2">
+              {{park.neighborhood}}
+            </div>
+          </v-card-text>
+        </v-card>
+      </v-hover>
+    </v-flex>
+  </v-layout>
+</v-container>
+
+
+  <!-- <v-container fluid>
     <v-layout row wrap justify-space-around
       class="parks"
       v-for="(park) in parks"
@@ -38,7 +108,7 @@
           </v-layout>
         </v-flex>
     </v-layout>
-  </v-container>
+  </v-container> -->
     
 </template>
 
