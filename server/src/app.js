d62e4fb5c51837a9f139cf6fbf48d0ce3e4d724d
hankwarner <40283259+hankwarner@users.noreply.github.com>
2019-03-25 22:45:32 -0400

    cors wip

--- server/src/app.js
+++ server/src/app.js
@@ -12,11 +12,12 @@ app.use(morgan('combined'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next()
-})
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+  next();
+});
 config.init(app, express);
 
 require('./routes')(app);
